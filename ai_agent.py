41d08baf1f74a4ebed14b3f3cd6728e17c764ce7
Laxit Sharma <160541839+laxitsharma@users.noreply.github.com>
2025-05-15 06:20:43 +0530

    Add files via upload

---
+++ ai_agent.py
@@ -0,0 +1,77 @@
+# if you dont use pipenv uncomment the following:
+from dotenv import load_dotenv
+load_dotenv()
+
+#Step1: Setup API Keys for Groq, OpenAI and Tavily
+import os
+
+GROQ_API_KEY=os.environ.get("GROQ_API_KEY")
+TAVILY_API_KEY=os.environ.get("TAVILY_API_KEY")
+OPENAI_API_KEY=os.environ.get("OPENAI_API_KEY")
+
+#Step2: Setup LLM & Tools
+from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+openai_llm=ChatOpenAI(model="gpt-4o-mini")
+groq_llm=ChatGroq(model="llama-3.3-70b-versatile")
+
+search_tool=TavilySearchResults(max_results=2)
+
+#Step3: Setup AI Agent with Search tool functionality
+from langgraph.prebuilt import create_react_agent
+from langchain_core.messages.ai import AIMessage
+
+system_prompt="You are an AI assistant for AIIMS Jodhpur Hospital. Your role is to guide new patients and visitors by providing directions within the hospital, locating doctor cabins and departments, and checking doctor availability. Always respond concisely and politely"
+
+def get_response_from_ai_agent(llm_id, query, allow_search, system_prompt, provider):
+    if provider=="Groq":
+        llm=ChatGroq(model=llm_id)
+    elif provider=="OpenAI":
+        llm=ChatOpenAI(model=llm_id)
+
+    tools=[TavilySearchResults(max_results=2)] if allow_search else []
+    agent=create_react_agent(
+        model=llm,
+        tools=tools,
+        state_modifier=system_prompt
+    )
+    state={"messages": query}
+    response=agent.invoke(state)
+    messages=response.get("messages")
+    ai_messages=[message.content for message in messages if isinstance(message, AIMessage)]
+    return ai_messages[-1]
+
+
+
+
+
+
+
+
+
+
+#     agent=create_react_agent(
+#         model=llm,
+#         tools=tools,
+#         state_modifier=system_prompt
+#     )
+#     state={"messages": query}
+#     response=agent.invoke(state)
+#     messages=response.get("messages")
+#     ai_messages=[message.content for message in messages if isinstance(message, AIMessage)]
+#     return ai_messages[-1]
+
+
+
+# if __name__ == "__main__":
+#     query = [{"role": "user", "content": "hi, how are you?"}]
+#     response = get_response_from_ai_agent(
+#         llm_id="llama3-70b-8192",  # or "gpt-4o-mini"
+#         query=query,
+#         allow_search=False,
+#         system_prompt=system_prompt,
+#         provider="Groq"  # or "OpenAI"
+#     )
+#     print("AI Response:", response)
---
+++ frontend.py
@@ -0,0 +1,54 @@
+# if you dont use pipenv uncomment the following:
+from dotenv import load_dotenv
+load_dotenv()
+
+
+#Step1: Setup UI with streamlit (model provider, model, system prompt, web_search, query)
+import streamlit as st
+
+st.set_page_config(page_title="LangGraph Agent UI", layout="centered")
+st.title("AI Chatbot Agents")
+st.write("Create and Interact with the AI Agents!")
+
+system_prompt="You are an AI assistant for AIIMS Jodhpur Hospital. Your role is to guide new patients and visitors by providing directions within the hospital, locating doctor cabins and departments, and checking doctor availability. Always respond concisely and politely"
+
+MODEL_NAMES_GROQ = ["llama-3.3-70b-versatile", "mixtral-8x7b-32768"]
+MODEL_NAMES_OPENAI = ["gpt-4o-mini"]
+
+provider=st.radio("Select Provider:", ("Groq", "OpenAI"))
+
+if provider == "Groq":
+    selected_model = st.selectbox("Select Groq Model:", MODEL_NAMES_GROQ)
+elif provider == "OpenAI":
+    selected_model = st.selectbox("Select OpenAI Model:", MODEL_NAMES_OPENAI)
+
+allow_web_search=st.checkbox("Allow Web Search")
+
+user_query=st.text_area("Enter your query: ", height=150, placeholder="Ask Anything!")
+
+API_URL="http://127.0.0.1:9999/chat"
+
+if st.button("Ask Agent!"):
+    if user_query.strip():
+        #Step2: Connect with backend via URL
+        import requests
+
+        payload={
+            "model_name": selected_model,
+            "model_provider": provider,
+            "system_prompt": system_prompt,
+            "messages": [user_query],
+            "allow_search": allow_web_search
+        }
+
+        response=requests.post(API_URL, json=payload)
+        if response.status_code == 200:
+            response_data = response.json()
+            if "error" in response_data:
+                st.error(response_data["error"])
+            else:
+                st.subheader("Agent Response")
+                st.markdown(f"**Final Response:** {response_data}")
+
+
+
